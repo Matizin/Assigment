802cc5ddf89d4793158bd7c3ea615fe697768dc3
Mateus <mateuscgr@gmail.com>
2022-11-03 14:43:11 -0300

    Finished after lots of crying

--- src/Crocodile.java
+++
@@ -1,9 +0,0 @@
-public class Crocodile extends Reptile {
-
-    public Crocodile(int height, int weight, String type, String blood, String phisycal) {
-        super(height, weight, type, blood, phisycal);
-        this.phisycal = Reptile.this.phisycal;
-    }
-    
-    void ShowInfo();
-}
--- src/Eagle.java
+++
@@ -1,8 +0,0 @@
-public class Eagle extends Birds {
-
-    public Eagle() {
-        super();
-        return Eagle[showInfo(super(Animal.Birds))];
-    }
-
-    
